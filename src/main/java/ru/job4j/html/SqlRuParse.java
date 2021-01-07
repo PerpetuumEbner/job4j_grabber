279d9423c52fe0170abeadea31288e83c31e232a
PerpetuumEbner <yustas@protonmail.com>
2021-01-07 22:27:53 +0500

    3.6.2.1.1. Парсинг sql.ru.

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -7,14 +7,28 @@ import org.jsoup.select.Elements;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        for (Element td : row) {
-            Element href = td.child(0);
-            String date = td.parent().child(5).text();
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
-            System.out.println(date);
+        String url = "https://www.sql.ru/forum/job-offers/";
+        int index = 1;
+        int amount = 0;
+
+        while (index < 6) {
+            String string = url.replace(url, url + index);
+            index++;
+
+            Document doc = Jsoup.connect(string).get();
+            Elements row = doc.select(".postslisttopic");
+            for (int i = 3; i < row.size(); i++) {
+                Element td = row.get(i);
+                Element href = td.child(0);
+                String date = td.parent().child(5).text();
+                System.out.println(href.attr("href"));
+                System.out.println(href.text());
+                System.out.println(date);
+                System.out.println();
+                amount++;
+            }
         }
+
+        System.out.println("Общее количество вакансий: " + amount);
     }
 }
