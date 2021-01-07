cfe563854ac7ef111d0ee20a5ba8106816ea9d5e
PerpetuumEbner <yustas@protonmail.com>
2021-01-07 23:03:28 +0500

    3.6.2.2. Модель данных - Post.

---
+++ src/main/java/ru/job4j/html/Post.java
@@ -0,0 +1,70 @@
+package ru.job4j.html;
+
+import java.sql.Timestamp;
+import java.util.Objects;
+
+public class Post {
+    private String title;
+    private String url;
+    private String description;
+    private Timestamp date;
+
+    public Post(String title, String url, String description, Timestamp date) {
+        this.title = title;
+        this.url = url;
+        this.description = description;
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Timestamp getDate() {
+        return date;
+    }
+
+    public void setDate(Timestamp date) {
+        this.date = date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Post post = (Post) o;
+        return Objects.equals(title, post.title)
+                && Objects.equals(url, post.url)
+                && Objects.equals(description, post.description)
+                && Objects.equals(date, post.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, url, description, date);
+    }
+}
